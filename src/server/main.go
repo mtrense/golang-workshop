9826e91fdc00c5d08e7b499973fe4381838d2137
Max Trense <dev@trense.info>
2018-08-01 09:55:56 +0200

    First version of echo webservice

--- src/server/main.go
+++ src/server/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
-	fmt.Println("Hello CLΛRK!")
+	server := echo.New()
+	server.GET("/", helloWorld)
+	server.Start(":1234")
+}
+
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello CLΛRK!")
 }
